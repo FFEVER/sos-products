fb290ce68a14175569b86011a3ec4095a67529b2
Nattaphol <rootncdev2015@gmail.com>
2020-04-14 00:10:33 +0700

    [~] Deploy to heroku without docker

--- .travis/deploy_heroku.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-curl https://cli-assets.heroku.com/install.sh | sh
-heroku container:login
-heroku container:push web --app $HEROKU_APP_NAME
-heroku container:release web --app $HEROKU_APP_NAME
