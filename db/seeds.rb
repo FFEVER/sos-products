e4db6d2478c64263cbb03c389bba5fff8856b12c
Nattaphol <rootncdev2015@gmail.com>
2020-04-19 21:02:48 +0700

    [+] Add seeded products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+p1 = Product.create(
+  title: "Product one of user 1",
+  user_id: 1,
+  long_desc: "This is my product one of user 1.",
+  price: 50,
+  stock: 300,
+  sold_quantity: 10,
+)
+puts "#{p1.title} created!"
+
+p2 = Product.create(
+  title: "Product two of user 1",
+  user_id: 1,
+  long_desc: "This is my product two of user 1.",
+  price: 30000,
+  stock: 20,
+  sold_quantity: 0,
+)
+puts "#{p2.title} created!"
+
+p3 = Product.create(
+  title: "Product one of user 2",
+  user_id: 2,
+  long_desc: "This is my product one of user 2.",
+  price: 20,
+  stock: 50000,
+  sold_quantity: 20,
+)
+puts "#{p3.title} created!"
