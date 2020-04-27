6382c7e0f3599e8d8523c0bf79689f196d31cc29
Nattaphol <rootncdev2015@gmail.com>
2020-04-28 01:41:20 +0700

    [+] Add category tree

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,11 @@
 class Category < ApplicationRecord
-    has_many :subcategories, class_name: 'Category', foreign_key: 'parent_id', dependent: :destroy
-    belongs_to :parent, class_name: 'Category', optional: true
-    has_and_belongs_to_many :products
+  has_many :subcategories, class_name: 'Category', foreign_key: 'parent_id', dependent: :destroy
+  belongs_to :parent, class_name: 'Category', optional: true
+  has_and_belongs_to_many :products
+  has_many :subproducts, through: :subcategories, source: :products
+
+  def all_products
+    self.products | self.subproducts
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ApplicationRecord
+    has_and_belongs_to_many :categories
+
+    def category_tree
+        category = self.categories.first
+        if category.parent
+            parent = category.parent.attributes
+            parent["subcategories"] = category.attributes
+        else
+            parent = category.attributes
+            parent["subcategories"] = {}
+        end
+        parent
+    end
+
 end
