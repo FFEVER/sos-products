23780474054da79131a89c8b7f9d2bc5cb27b839
Nattaphol <rootncdev2015@gmail.com>
2020-04-28 03:26:15 +0700

    [+] Add category to product in controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,21 @@
 class Product < ApplicationRecord
-    has_and_belongs_to_many :categories
+  has_and_belongs_to_many :categories
 
-    def category_tree
-        category = self.categories.first
-        if category.parent
-            parent = category.parent.attributes
-            parent["subcategories"] = category.attributes
-        else
-            parent = category.attributes
-            parent["subcategories"] = {}
-        end
-        parent
+  def category_tree
+    category = categories.first
+    if category.parent
+      parent = category.parent.attributes
+      parent['subcategories'] = category.attributes
+    else
+      parent = category.attributes
+      parent['subcategories'] = {}
     end
+    parent
+  end
+
+  def category_id=(id)
+    categories.destroy_all
+    categories << Category.find(id)
+  end
 
 end
