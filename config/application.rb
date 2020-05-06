41b7261b9b81ecf40dd21f04cfaab8bda3fafcec
Nattaphol <rootncdev2015@gmail.com>
2020-05-06 20:42:22 +0700

    [~] Allow any domain

--- config/application.rb
+++ config/application.rb
@@ -36,8 +36,8 @@ module SOSProducts
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000', 'https://sos-products.herokuapp.com/'
-        resource '*', :headers => :any, :methods => [:get]
+        origins '*'
+        resource '*', headers: :any, methods: [:get]
       end
     end
   end
